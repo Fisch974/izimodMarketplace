10adfc44e58910e0d452ee131e802a3703918df9
Payet_michel <michel.payet974@live.fr>
2025-04-22 11:26:33 +0200

    correction page d'accueil et ajout d'une page product

--- mon_projet/src/js/pages/App.jsx
+++ mon_projet/src/js/pages/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from './Admin_page.jsx';
 import Login from './Login_page.jsx';
 import User from './User_account.jsx';
+import Product from './product.jsx';
+
 
 // Components
 import Header from '../components/Header.jsx';
@@ -12,12 +14,13 @@ import Header from '../components/Header.jsx';
 function App() {
   return (
 <app>
-    <Header></Header>
+    <Header />
     <BrowserRouter> {/*Integrated routing to redirect links */}
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Add link with path*/}
+        <Route path="/" element={<Product />} /> {/* Add link with path*/}
         <Route path="/admin" element={<Admin />} />
         <Route path="/users" element={<User />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
 </app>
---
+++ mon_projet/src/js/pages/product.jsx
@@ -0,0 +1,11 @@
+import '../bootstrap.js';
+import React from 'react';
+
+function Product(params) {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Product;
