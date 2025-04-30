b46f9571ee0796e545179e63d48428497dd78b47
jerem.re <114880829+jerem-point-re@users.noreply.github.com>
2025-04-30 14:42:47 +0400

    Header okay (desk, mob, tab)

--- mon_projet/src/js/components/Header.jsx
+++ mon_projet/src/js/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useState } from "react";
 // Librairie d'icônes
 import {
@@ -7,21 +8,24 @@ import {
   LogOut,
   Menu,
   PanelTopClose,
-  PanelBottomOpen,
+  Search,
   User,
-  X
+  X,
+  PanelTopOpen
 } from "lucide-react";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   // useState pour gérer l'état (ouvert/fermé) du menu
   const [isOpen, setIsOpen] = useState(false);
-  // 
-  const [switchIcon, setSwitchIcon] = useState(false);
   // Fonction fléchée qui altère l'état du menu (du useState)
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    setSwitchIcon(!switchIcon);
+  }
+  // Trigger l'icône pour affichage ou non de la deuxième
+  const [isClicked, setIsClicked] = useState(false);
+  // Fonction fléchée qui altère l'état du menu (du useState)
+  const toggleClick = () => {
+    setIsClicked(!isClicked);
   }
 
   const notImplemented = () => {
@@ -30,44 +34,68 @@ const Header = () => {
 
   return (
     <>
-      <header className="bg-white shadow fixed-top">
-        <SubHeader />
+      <header className="bg-white shadow stick-top">
+        {isClicked ? "": <SubHeader />}
         <nav className="navbar navbar-expand-md container py-2">
-          <Link to="/"><img src="../../data/logo/iconBlue.svg" width={64} height={64} className="ms-3" /></Link>
-          <Link to='/' className="navbar-link link fw-bold fs-5 ms-md-3 d-md-block d-none">
-            iziMOD | VOTRE Marketplace
-          </Link>
-          <button
-            className="bg-white me-3 text-black border-0 mb-3 d-md-none"
-            onClick={toggleMenu}
-            aria-label="Ouvrir le menu"
-          >
-            <span className="d-md-none">
-              {switchIcon && isOpen ? <X /> : <Menu />}
-            </span>
-          </button>
-          <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
-            <ul className="navbar-nav ms-md-auto me-5 text-center mb-3 mb-md-0">
-              <li className="navbar-brand my-3 w-100">
-                <Link to="/" className="navbar-link link d-md-none fw-bold">iziMOD | VOTRE Marketplace</Link>
-              </li>
-              <li className="nav-item w-100">
-                <a className="nav-link" href="login">
-                  < User /> <span className="d-md-none">Connexion</span>
-                </a>
-              </li>
-              <li className="nav-item w-100" onClick={notImplemented}>
-                <a className="nav-link" href="#">
-                  <LogOut /> <span className="d-md-none">Déconnexion</span>
-                </a>
-              </li>
-            </ul>
+          <div className="container-fluid d-flex align-items-center justify-content-between">
+
+            <div className="d-flex align-items-center">
+              <Link to="/">
+                <img src="../../data/logo/iconBlue.svg" width={64} height={64} className="me-2" alt="Logo" />
+              </Link>
+              <Link to="/" className="navbar-link link fw-bold fs-5 d-none d-md-block">
+                iziMOD<span className="d-lg-inline d-md-none"> | VOTRE Marketplace</span>
+              </Link>
+            </div>
+
+            <form className="d-flex flex-row align-items-center w-50" id="searchForm" role="search">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Rechercher..."
+                aria-label="Rechercher..."
+              />
+              <a className="me-2 text-dark" id="searchBtn" type="submit">
+                <Search />
+              </a>
+            </form>
+
+            <div className="d-flex align-items-center justify-content-center">
+              <button
+                className="bg-white text-black border-0 d-md-none"
+                onClick={toggleMenu}
+                aria-label="Ouvrir le menu"
+              >
+                {isOpen ? <X className="mb-4" /> : <Menu className="mb-2" />}
+              </button>
+
+
+              <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
+                <ul className="navbar-nav text-center mb-3 mb-md-0 d-md-flex flex-row gap-3 navbar-nav-scroll">
+                  <li className="nav-item">
+                    <a className="nav-link" href="login">
+                      <User /> <span className="d-md-none">Connexion</span>
+                    </a>
+                  </li>
+                  <li className="nav-item" onClick={notImplemented}>
+                    <a className="nav-link" href="#">
+                      <LogOut /> <span className="d-md-none">Déconnexion</span>
+                    </a>
+                  </li>
+                  <li className="nav-item" onClick={toggleClick}>
+                    <a className="nav-link" href="#">
+                      {isClicked ? <PanelTopOpen /> : <PanelTopClose />} <span className="d-md-none">Afficher/Masquer la barre du haut</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </nav>
       </header>
     </>
   );
-};
+}
 
 const SubHeader = () => {
   return <nav className="navbar bg-primary d-flex flex-md-row flex-column">
