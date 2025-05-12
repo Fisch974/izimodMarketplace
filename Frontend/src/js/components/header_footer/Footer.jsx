1b2bdb68b7733f879b030e94ea96af81388f3b02
jerem.re <114880829+jerem-point-re@users.noreply.github.com>
2025-05-12 15:50:21 +0400

    RGPD générée automatiquement, relire pour confirmation

--- Frontend/src/js/components/header_footer/Footer.jsx
+++ Frontend/src/js/components/header_footer/Footer.jsx
@@ -1,16 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import React from 'react';
-import '../bootstrap.js';
-
+import '../../bootstrap.js';
 
 // Footer component to display the footer of the page
 // This component is responsible for showing the footer links and copyright information
 // It includes links to privacy policy, terms of use, about page, and contact page
-function Footer_page() {
+function Footer() {
     return (
         <footer className="text-white py-4 mt-auto footer">
             <div className="container text-center">
                 <div className="mb-3">
-                    <a href="/privacy" className="text-white mx-3 footer-link">Politique de confidentialité</a>
+                    <a href="./js/pages/legal/Privacy.html" className="text-white mx-3 footer-link">Politique de confidentialité</a>
                     <a href="/terms" className="text-white mx-3 footer-link">Conditions d'utilisation</a>
                     <a href="/about" className="text-white mx-3 footer-link">À propos</a>
                     <a href="/contact" className="text-white mx-3 footer-link">Contact</a>
@@ -21,4 +21,4 @@ function Footer_page() {
     );
 }
 
-export default Footer_page;
+export default Footer;
